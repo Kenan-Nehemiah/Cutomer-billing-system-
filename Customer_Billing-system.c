665564bd5821a5c3d4c527e0a4e3fd4c77f05e9e
The_Ring_coder <104777331+Kenan-Nehemiah@users.noreply.github.com>
2022-07-07 11:37:36 -0700

    Billing

---
+++ Customer_Billing-system.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+main void input()
+{
+    printf("CUSTOMER BILLING SYSTEM:\n");
+    printf("\n");
+    printf("========================\n");
+    printf("1. Add an account\n");
+    printf("2. Search for an account\n") ;
+    printf("3. Exit\n");
+
+    printf("where do you want to go?\n");
+       
+
+
+}
